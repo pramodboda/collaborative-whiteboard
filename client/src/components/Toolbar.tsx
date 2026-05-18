65813b619a0c0312a5c3f25d6cd9ed246b846ff9
Pramod Boda <pramodkumarboda@gmail.com>
2026-05-18 22:13:46 +0000

    Updating UI

--- client/src/components/Toolbar.tsx
+++ client/src/components/Toolbar.tsx
@@ -3,6 +3,14 @@
 import { socket } from "../hooks/useSocket";
 import { useBoardStore } from "../store/boardStore";
 
+import ButtonGroup from "@mui/material/ButtonGroup";
+import Button from "@mui/material/Button";
+
+import { BsPen } from "react-icons/bs";
+import { LuUndo } from "react-icons/lu";
+import { LuRedo } from "react-icons/lu";
+import { MdOutlineCleaningServices } from "react-icons/md";
+
 const roomId = "room-1";
 
 const Toolbar = () => {
@@ -39,52 +47,92 @@ const Toolbar = () => {
       element: restored,
     });
   };
+
   const handleClearBoard = () => {
     clearBoard();
     socket.emit("clear-board", roomId);
   };
 
   return (
-    <div
-      style={{
-        display: "flex",
-        gap: 10,
-        padding: 10,
-        // position: "fixed",
-        top: 10,
-        left: 10,
-        background: "white",
-        zIndex: 1000,
-      }}
-    >
-      <button onClick={() => setTool("pencil")}>Pencil</button>
+    <div>
+      <div
+        style={{
+          display: "flex",
+          gap: 10,
+          padding: 10,
+          // position: "fixed",
+          top: 10,
+          left: 10,
+          background: "white",
+          zIndex: 1000,
+        }}
+      >
+        {/* 
+        <button onClick={() => setTool("pencil")}>pencil</button> 
 
-      <button onClick={() => setTool("rectangle")}>Rectangle</button>
+        <button onClick={() => setTool("rectangle")}>Rectangle</button>
 
-      <button onClick={() => setTool("circle")}>Circle</button>
+        <button onClick={() => setTool("circle")}>Circle</button>
 
-      <button onClick={() => setTool("line")}>Line</button>
+        <button onClick={() => setTool("line")}>Line</button> */}
 
-      <input
-        type="color"
-        value={color}
-        onChange={(e) => setColor(e.target.value)}
-      />
+        <input
+          type="color"
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+        />
 
-      <input
-        type="range"
-        min={1}
-        max={20}
-        value={size}
-        onChange={(e) => setSize(Number(e.target.value))}
-      />
+        <input
+          type="range"
+          min={1}
+          max={20}
+          value={size}
+          onChange={(e) => setSize(Number(e.target.value))}
+        />
 
-      <button onClick={handleUndo}>Undo</button>
+        {/* <button onClick={handleUndo}>Undo</button> */}
+        {/* <button onClick={handleRedo}>Redo</button> */}
+        {/* <button onClick={handleClearBoard}>Clear</button> */}
 
-      <button onClick={handleRedo}>Redo</button>
-      <button onClick={handleClearBoard}>Clear</button>
+        <div>{tool}</div>
+      </div>
+      <ButtonGroup>
+        <Button
+          aria-label="pen"
+          onClick={() => setTool("pencil")}
+          startIcon={<BsPen style={{ fontSize: "1rem", fontWeight: "bold" }} />}
+        >
+          Pen
+        </Button>
+        <Button aria-label="rectangle" onClick={() => setTool("rectangle")}>
+          Rectangle
+        </Button>
 
-      <div>{tool}</div>
+        <Button aria-label="circle" onClick={() => setTool("circle")}>
+          Circle
+        </Button>
+
+        <Button aria-label="line" onClick={() => setTool("line")}>
+          Line
+        </Button>
+      </ButtonGroup>
+      <ButtonGroup>
+        <Button aria-label="undo" onClick={handleUndo} startIcon={<LuUndo />}>
+          Undo
+        </Button>
+        <Button aria-label="redo" onClick={handleRedo} startIcon={<LuRedo />}>
+          Redo
+        </Button>
+      </ButtonGroup>
+      <Button
+        variant="outlined"
+        color="error"
+        aria-label="clear-board"
+        onClick={handleClearBoard}
+        startIcon={<MdOutlineCleaningServices />}
+      >
+        Clear
+      </Button>
     </div>
   );
 };
