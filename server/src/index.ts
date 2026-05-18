46285ffca79e9e9ec1245bb1ba4f40af4a7c2ffb
Pramod Boda <pramodkumarboda@gmail.com>
2026-05-18 22:34:32 +0000

    Initial Project setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -20,6 +20,8 @@ const io = new Server(server, {
 
 registerWhiteboardSocket(io);
 
-server.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
